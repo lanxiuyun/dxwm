693a1768dbcd3a7d5a8f0d094b8e0094b8ddb73b
lanxiuyun <lanxiuyun310@gmail.com>
2026-04-28 17:16:16 +0800

    workflows

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/dxwm/',
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+  },
+  preview: {
+    host: '0.0.0.0',
+  },
   build: {
     minify: false,
     reportCompressedSize: false,
